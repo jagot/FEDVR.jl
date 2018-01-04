7754664d99afe6f727d04809cee3927adae89c01
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2018-01-05 00:30:35 +0100

    A few more unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,4 +17,26 @@ using Base.Test
     @test elems(grid) == 1:N-1
     @test order(grid) == n
     @test basecount(grid) == (N-1)*n - (N-2)
+    Xp = locs(grid)
+    @test length(Xp) == basecount(grid)
+    @test Xp[1] == breaks[1]
+    @test Xp[end] == breaks[end]
+
+    println(grid)
+
+    using RecipesBase
+    RecipesBase.apply_recipe(Dict{Symbol,Any}(), grid)
+end
+
+@testset "lagrange" begin
+    N = 11
+    n = 5
+    breaks = linspace(0,1,N)
+    grid = FEDVR.Grid(breaks, n)
+    e = m -> [zeros(m-1);1;zeros(n-m)]
+    for i = elems(grid)
+        for m = 1:n
+            @test FEDVR.lagrange(grid.X[i,:], m, grid.X[i,:]) == e(m)
+        end
+    end
 end
