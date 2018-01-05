5de89c61be27c3f9309dcf1163b8a7e606b22c4e
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2018-01-05 11:39:30 +0100

    Unit tests of Lagrange interpolating polynomial derivatives

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,4 +40,27 @@ end
             @test FEDVR.lagrange(grid.X[i,:], m, grid.X[i,:]) == e(m)
         end
     end
+
+    @test FEDVR.δ(-1, -1) == 1
+    @test FEDVR.δ(-1, 1) == 0
+    @test FEDVR.δ(0, 0) == 1
+    @test FEDVR.δ(0, 1) == 0
+
+    f = x -> x^4
+    g = x -> 4x^3
+
+    function vecdist(a::AbstractVector, b::AbstractVector,
+                     ϵ = eps(eltype(a)))
+        δ = √(sum(abs2, a-b))
+        δ, δ/√(sum(abs2, a .+ ϵ))
+    end
+
+    for i = elems(grid)
+        L′ = zeros(n,n)
+        FEDVR.lagrangeder!(grid.X[i,:], grid.W[i,:], L′)
+        c = f.(grid.X[i,:])
+        c′ = L′'c
+        c′a = g.(grid.X[i,:])
+        @test vecdist(c′, c′a)[2] < 1e-13
+    end
 end
