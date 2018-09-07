68fd636a76a792c19849dff6721fa36e5c1b2dc6
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2018-09-07 11:59:48 +0200

    Fixed deprecated sparse matrix construction

--- src/potentials.jl
+++ src/potentials.jl
@@ -2,7 +2,7 @@ using LinearMaps
 
 function potop(basis::Basis, V::Function)
     Vv = V.(locs(basis.grid))
-    LinearMap(spdiagm(Vv), isposdef=all(Vv .> 0))
+    LinearMap(spdiagm(0 => Vv), isposdef=all(Vv .> 0))
 end
 
 export potop
