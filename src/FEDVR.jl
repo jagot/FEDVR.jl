15df79fea83d885f836ddd7d333bae2d0983acc0
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2020-05-17 11:34:12 +0200

    Added deprecation notice

--- src/FEDVR.jl
+++ src/FEDVR.jl
@@ -2,6 +2,11 @@ __precompile__()
 
 module FEDVR
 
+function __init__()
+    @warn "The FEDVR.jl package has been deprecated in favour of JuliaApproximation/CompactBases.jl"
+    nothing
+end
+
 using SparseArrays
 using RecipesBase
 
